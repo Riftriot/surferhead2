1249486596ec618c0a8a16e9ce827e9affe15ff9
Mikkel RINGAUD <mikkel@milescode.dev>
2023-11-26 23:01:57 +0100

    chore: always `allowMultipleWindows`

--- src/client/utils/get-corrected-target-for-single-page-mode.ts
+++ src/client/utils/get-corrected-target-for-single-page-mode.ts
@@ -1,10 +1,10 @@
-import settings from '../settings';
+// import settings from '../settings';
 import isKeywordTarget from '../../utils/is-keyword-target';
 import * as windowsStorage from '../sandbox/windows-storage';
 
 export default function (target = ''): string {
-    if (settings.get().allowMultipleWindows)
-        return target;
+    // if (settings.get().allowMultipleWindows)
+    return target;
 
     if (target && !isKeywordTarget(target) && !windowsStorage.findByName(target) ||
         /_blank/i.test(target))
