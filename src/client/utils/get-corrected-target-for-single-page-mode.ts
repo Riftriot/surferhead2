b04acc447f6d0da7d9edb5cee1432c5367181660
Mikkel RINGAUD <mikkel@milescode.dev>
2023-11-27 19:04:35 +0100

    fix(target): proxy when _blank

--- src/client/utils/get-corrected-target-for-single-page-mode.ts
+++ src/client/utils/get-corrected-target-for-single-page-mode.ts
@@ -3,9 +3,6 @@ import isKeywordTarget from '../../utils/is-keyword-target';
 import * as windowsStorage from '../sandbox/windows-storage';
 
 export default function (target = ''): string {
-    // if (settings.get().allowMultipleWindows)
-    return target;
-
     if (target && !isKeywordTarget(target) && !windowsStorage.findByName(target) ||
         /_blank/i.test(target))
         return 'surferhead_top_proxied_iframe';
